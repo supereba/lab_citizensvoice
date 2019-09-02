f525202db250e624ff61271eb8a9775ec9886ea3
Emmanuel Brun D'Aubignosc <emmanuel@brun>
2019-09-02 16:51:25 +0200

    premiere revision

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  private baseUrl = 'http://localhost:8080/springboot-crud-rest/api/v1/employees';
+  private baseUrl = 'http://localhost:8080/api/v1/employees';
 
   constructor(private http: HttpClient) { }
 
